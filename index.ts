779066e112a97a296af783ca8ad16d56b3053f9f
luthandodidizav2 <didizaluthando197@gmail.com>
2025-11-17 15:08:46 +0200

    Remove node_modules from repo and add to .gitignore 2

--- index.ts
+++ index.ts
@@ -6,9 +6,12 @@ import morgan from 'morgan';
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet()); 
+
 app.use(cors());
+
 app.use(express.json());
+
 app.use(morgan('dev'));
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
